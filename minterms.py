334c4a156cda20bd6e15a4a2ae22da5f8337c29e
AbelJuncal <abel.juncal@udc.es>
2023-03-03 13:33:21 +0100

    Add first approach

---
+++ minterms.py
@@ -0,0 +1,55 @@
+
+def lista_primos(dicImplicantes):
+    listaPrimos = []
+    for x , y in dicImplicantes.items():
+        if y == 0:
+            listaPrimos.append((x,y))
+
+    return listaPrimos
+
+def stringApproach():
+    entrada = [("001", 0),("101", 0),("110", 0),("111", 0)]
+    salida = {}
+
+    while(entrada):
+        print("\nNueva iteracion\n")
+        aux_entrada = dict(entrada)
+        listaFusion = {}
+
+        print("entrada", aux_entrada, "\n")
+
+        #TODO como miro si se pueden relacionar más, mirar si es primo, guardar de que implicantes salen
+        for i in range(0, len(entrada)-1):
+            for j in range(i+1, len(entrada)):
+                first = [*entrada[i][0]]
+                second = [*entrada[j][0]]
+
+                diferencia = 0
+                fusion = ""
+
+                for z in range(len(first)):
+                    if first[z] == second[z]:
+                        fusion = fusion + first[z]
+                    else:
+                        fusion = fusion + "-"
+                        diferencia+=1
+
+                print("pareja", first, second)
+                
+                if(diferencia == 1):
+                    listaFusion.update({fusion: 0})
+                    aux_entrada.update({entrada[i][0]: 1})
+                    aux_entrada.update({entrada[j][0]: 1})
+                    print("fusion",fusion)
+
+        print("primos", lista_primos(aux_entrada))
+        salida.update(dict(lista_primos(aux_entrada)))
+        
+        print("listaFusion", listaFusion)
+        print("\n")
+        entrada = list(listaFusion.items())
+
+    print(list(salida.items()))
+
+if __name__ == "__main__":
+    stringApproach()
