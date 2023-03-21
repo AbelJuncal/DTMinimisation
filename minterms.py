7bcbc4ea4aa61e2f4870db2b9403e70591378468
AbelJuncal <abel.juncal@udc.es>
2023-03-21 12:03:07 +0100

    Translate all code

--- minterms.py
+++ minterms.py
@@ -8,61 +8,61 @@ class BooleanImplicant(Implicant):
 
     def matches(self, otherImplicant):
     
-        diferencia = 0
-        fusion = ""
+        difference = 0
+        merging = ""
 
         for z in range(len(self.string)):
             if self.string[z] == otherImplicant.string[z]:
-                fusion = fusion + self.string[z]
+                merging = merging + self.string[z]
             else:
-                fusion = fusion + "-"
-                diferencia+=1
+                merging = merging + "-"
+                difference+=1
 
-        if(diferencia == 1):
-            return BooleanImplicant(fusion)
+        if(difference == 1):
+            return BooleanImplicant(merging)
         
         return None
 
     def __str__ (self):
         return self.string
 
-def lista_primos(dicImplicantes):
-    listaPrimos = []
-    for x , y in dicImplicantes.items():
+def primeImplicants(dicImplicants):
+    primeList = []
+    for x , y in dicImplicants.items():
         if y == 0:
-            listaPrimos.append(x)
+            primeList.append(x)
 
-    return listaPrimos
+    return primeList
 
-def stringApproach(entrada):
+def stringApproach(input):
 
-    salida = set()
+    output = set()
 
-    while(entrada):
-        aux_entrada = list_to_dictionary(entrada)
-        listaFusion = []
+    while(input):
+        auxInput = list_to_dictionary(input)
+        mergingList = []
 
-        for i in range(0, len(entrada)-1):
-            for j in range(i+1, len(entrada)):
-                first = entrada[i]
-                second = entrada[j]
+        for i in range(0, len(input)-1):
+            for j in range(i+1, len(input)):
+                first = input[i]
+                second = input[j]
 
                 fusion = first.matches(second)
                 
                 if fusion:
-                    listaFusion.append(fusion)
-                    aux_entrada[first]=1
-                    aux_entrada[second]=1
+                    mergingList.append(fusion)
+                    auxInput[first]=1
+                    auxInput[second]=1
 
-        salida.update(lista_primos(aux_entrada))
+        output.update(primeImplicants(auxInput))
         
-        entrada = listaFusion
+        input = mergingList
 
-    return salida
+    return output
 
-def printOutput(entrada, salida):
-    print("entrada", ' '.join(map(str, entrada))) 
-    print("salida", ' '.join(map(str, salida)))
+def printOutput(input, output):
+    print("input", ' '.join(map(str, input))) 
+    print("output", ' '.join(map(str, output)))
 
 
 def list_to_dictionary(list):
@@ -74,11 +74,11 @@ def list_to_dictionary(list):
     return dict
 
 def main():
-    entrada = [BooleanImplicant("001"),BooleanImplicant("101"), BooleanImplicant("110"), BooleanImplicant("111")]
+    input = [BooleanImplicant("001"),BooleanImplicant("101"), BooleanImplicant("110"), BooleanImplicant("111")]
 
-    salida = stringApproach(entrada)
+    output = stringApproach(input)
 
-    printOutput(entrada, salida)
+    printOutput(input, output)
 
 
 if __name__ == "__main__":
