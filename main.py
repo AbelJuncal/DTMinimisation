de1d1483f5faa4c4ccde0442c3096c01840342ee
AbelJuncal <abel.juncal@udc.es>
2023-05-05 13:00:40 +0200

    Change main to receive file as parameter

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from Resources.readInput import *
 from Resources.intervalsGenerator import *
 from quineMcCluskey import *
+import sys
 
 def main():
-    generator = IntervalsGenerator()
 
-    generatedImplicants = generator.generate()
+    readInput = ReadInput()
 
-    print("stages:", generator.stages)
-    print("implicants: ", '\n'.join(map(str, generatedImplicants)))
+    input = readInput.readIntervalInput("GeneratedExamples/" + sys.argv[1])
+    
+    output = QuineMcCluskey.execute(input)
+    
+    Resources.printOutput(input, output)
+
+    filename = sys.argv[1]
+
+    print(filename)
+
+    with open('PyOutput/' + filename, 'w') as f:
+        for implicant in list(output):
+            for tuple in implicant.list:
+                    for number in tuple:
+                        f.write(str(number) + " ")
+            f.write("\n")
+
 
 if __name__ == "__main__":
     main()
