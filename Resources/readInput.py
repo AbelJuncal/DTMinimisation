922d7e74b5cc4b036eb45e4ccf57ec0cdb18c80b
AbelJuncal <abel.juncal@udc.es>
2023-07-27 12:22:47 +0200

    Add read for optimization input

--- Resources/readInput.py
+++ Resources/readInput.py
@@ -1,5 +1,6 @@
 from Implicant.booleanImplicant import *
 from Implicant.intervalmplicant import *
+from Implicant.optimizedintervalImplicant import *
 
 class ReadInput():
     @classmethod
@@ -48,6 +49,49 @@ class ReadInput():
                 tuples.append(nuevatupler)
 
             input.append(IntervalImplicant(tuples))
+
+        for i in range(numVariables):
+            setordenado = sorted(setsList[i])
+            setsList[i] = setordenado
+
+        self.stages = setsList
+
+        return input
+    
+    @classmethod
+    def readOptimizedIntervalInput(self, filename):
+        file = open(filename)
+        lines = file.read().splitlines()
+        input = []
+        
+        numVariables = int(lines[0])
+        lines.pop(0)
+
+        setsList = []
+
+        for i in range(numVariables):
+            nuevoSetVariable = set()
+            nuevoSetVariable.update([0, int(lines[0])])
+            lines.pop(0)
+            setsList.append(nuevoSetVariable)
+
+        for element in lines:
+            x = element.split()
+            tuples = [] 
+            nueva_lista = [x[i:i+2] for i in range(0, len(x), 2)]
+
+            for i in range(numVariables):
+                lista = nueva_lista[i]
+                nuevatupler = []
+
+                for elementer in lista:
+                    setsList[i].add(int(elementer))
+                    nuevatupler.append(int(elementer))
+
+                nuevatupler = tuple(nuevatupler)
+                tuples.append(nuevatupler)
+
+            input.append(optimizedIntervalImplicant(tuples))
 
         for i in range(numVariables):
             setordenado = sorted(setsList[i])
