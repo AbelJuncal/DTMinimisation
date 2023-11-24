b54de168b5c078c55b230126499c2e0721fa8e78
AbelJuncal <abel.juncal@udc.es>
2023-11-24 02:11:49 +0100

    Change support inputs to ints

--- build.py
+++ build.py
@@ -35,18 +35,6 @@ def main():
     with open("tests/"+sys.argv[1]+"/times/optimizedTimes.txt") as f:
         optimized = f.read().splitlines()
 
-    with open("tests/"+sys.argv[1]+"/times/fc.txt") as f:
-        calculatefc = f.read().splitlines()
-
-    values = dict()
-
-    for l in calculatefc:
-        splitted = l.split(",")
-        example = splitted[0].split('.')[1]
-        fc = splitted[1]
-        mfc = splitted[2]
-        values[example] = [fc, mfc]
-
     for l in prolog:
         splitted = l.split(".")
         if splitted[2]!= '':
@@ -65,7 +53,6 @@ def main():
             minutes = splitted[2].split("m")[0]
             seconds = splitted[2].split("m")[1].split("s")[0].replace(",", ".")
             time = (int(minutes) * 60) + float(seconds)
-            print(splitted[1], minutes, seconds, time)
         else:
             time = 120
 
@@ -77,7 +64,6 @@ def main():
             minutes = splitted[2].split("m")[0]
             seconds = splitted[2].split("m")[1].split("s")[0].replace(",", ".")
             time = (int(minutes) * 60) + float(seconds)
-            print(splitted[1], minutes, seconds, time)
         else:
             time = 120
 
@@ -93,28 +79,23 @@ def main():
 
     with open("tests/"+sys.argv[1]+"/times/tiempos.csv", mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(["ejecución", "programa", "fc", "mfc", "ejemplo", "tiempo"])
+        writer.writerow(["ejecución", "programa", "ejemplo", "tiempo"])
         for t in prologList:
             for i in range(0, 5):
-                writer.writerow([i+1, "prolog", values.get(t[0])[0], values.get(t[0])[1], t[0], t[1][i]])
+                if len(t[1]) != 0:
+                    writer.writerow([i+1, "prolog", t[0], t[1][i]])
 
         for p in pythonList:
             for i in range(0,5):
-                writer.writerow([i+1, "python", values.get(p[0])[0], values.get(p[0])[1], p[0], p[1][i]])
+                if len(p[1]) != 0:
+                    writer.writerow([i+1, "python", p[0], p[1][i]])
 
         for o in optimizationList:
             for i in range(0,5):
-                writer.writerow([i+1, "optimized", values.get(o[0])[0], values.get(o[0])[1], o[0], o[1][i]])
+                if len(o[1]) != 0:
+                    writer.writerow([i+1, "optimized", o[0], o[1][i]])
 
     file.close()
-
-    # df = pd.read_csv('tests/'+sys.argv[1]+'/times/tiempos.csv')
-    # filtered = df.filter(items = ['programa'])
-
-    # ex = sns.load_dataset("penguins")
-    # sns.pairplot(ex, hue="species")
-
-    # plt.show()
 
 
 if __name__ == "__main__":
